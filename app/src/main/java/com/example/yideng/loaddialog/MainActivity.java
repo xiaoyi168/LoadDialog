ed548ad5a1bb4a8d77545d0228355f1c4251051a
Administrator <alijiahua@126.com>
2017-06-22 16:19:38 +0800

    修复4.0适配问题

--- app/src/main/java/com/example/yideng/loaddialog/MainActivity.java
+++ app/src/main/java/com/example/yideng/loaddialog/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.yideng.loaddialog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.yideng.loaddialoglibrary.LoadDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView= (TextView) findViewById(R.id.id_text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadDialog loadDialog=new LoadDialog(MainActivity.this);
+                loadDialog.ShowText(); //是否显示加载中字体（默认不显示）
+                loadDialog.SetText("努力加载中…");  //自定义字体
+                loadDialog.show();
+
+            }
+        });
+
     }
 }
