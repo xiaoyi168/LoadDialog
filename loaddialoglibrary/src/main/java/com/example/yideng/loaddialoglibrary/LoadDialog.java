ebb6b585e908ec478da8812433248dcf8980cf7d
Administrator <alijiahua@126.com>
2017-06-22 16:18:15 +0800

    修复4.0适配问题

--- loaddialoglibrary/src/main/java/com/example/yideng/loaddialoglibrary/LoadDialog.java
+++ loaddialoglibrary/src/main/java/com/example/yideng/loaddialoglibrary/LoadDialog.java
@@ -23,10 +23,10 @@ public class LoadDialog extends Dialog{
     private static   TextView mTextView;
 
     public LoadDialog(@NonNull Context context) {
-        super(context);
+        super(context,R.style.MyDialog);
         setContentView(R.layout.dialog);
-        Window window = getWindow();
-        window.setBackgroundDrawableResource(R.drawable.corners_radio);
+     /*   Window window = getWindow();
+        window.setBackgroundDrawableResource(R.drawable.corners_radio);*/
         ImageView img= (ImageView)findViewById(R.id.id_img);
         Rotate(img);
         mTextView = (TextView)findViewById(R.id.id_text);
